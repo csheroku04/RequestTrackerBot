6e215f3408d901dbe263f22e04d17c43fb909da5
cinerequa <97950615+cinerequa@users.noreply.github.com>
2023-04-23 15:30:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -314,9 +314,8 @@ async def requestHandler(bot:Update, msg:Message):
                 )
             )
 
-       replyText = f"<b>👋 Hello {mentionUser} !!\n\n📍 Your Request for {contentRequested} has been submitted to the admins.\n\n🚀 Your Request Will Be Uploaded soon.\n📌 Please Note that Admins might be busy. So, this may take more time.\n\n👇 See Your Request Status Here 👇</b>"
+            replyText = f"<b>👋 Hello {mentionUser} !!\n\n📍 Your Request for {contentRequested} has been submitted to the admins.\n\n🚀 Your Request Will Be Uploaded soon.\n📌 Please Note that Admins might be busy. So, this may take more time.\n\n👇 See Your Request Status Here 👇</b>"
 
-    
             # Sending message for user in group
             await msg.reply_text(
                 replyText,
@@ -326,7 +325,7 @@ async def requestHandler(bot:Update, msg:Message):
                     [
                         [
                             InlineKeyboardButton(
-                                "✅ Request Listed ✅",
+                                "⏳Request Status⏳",
                                 url = f"https://t.me/c/{channelIDPro}/{requestMSG.message_id}"
                             )
                         ]
